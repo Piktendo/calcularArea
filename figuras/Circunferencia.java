d2047f93b750fde133eca2d03ae65e8e39e84516
Carlos A <47528626+Piktendo@users.noreply.github.com>
2022-01-12 12:09:03 +0000

    Tercer commit del proyecto de Carlos Yllobre

---
+++ figuras/Circunferencia.java
@@ -0,0 +1,45 @@
+package figuras;
+
+public class Circunferencia {
+    private String color;
+
+    public double getRadio() {
+        return radio;
+    }
+
+    public void setRadio(double radio) {
+        this.radio = radio;
+    }
+
+    private double radio;
+
+    public Circunferencia(double radio) {
+        this.radio = radio;
+    }
+
+    public void imprimir() {
+        color = "rojo";
+        System.out.println("Diámetro: " + (2*radio));
+        System.out.println("Color: " + color);
+        double area1 = 2* 3.1416 *radio*radio;
+        double area = area1;
+        System.out.println(area);
+    }
+
+    public boolean esIgual(boolean conDecimales, Circunferencia otro) {
+        double radio1 = this.radio;
+        double radio2 = otro.getRadio();
+        if (conDecimales) {
+            if (radio1 == radio2)
+                return true;
+            else
+                return false;
+        } else {
+            if (Math.abs(radio1 - radio2) < 1)
+                return true;
+            else
+                return false;
+        }
+    }
+}
+
